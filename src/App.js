f238d2ff4518396ee29b0690a64edc0dac141bc8
Merilee Wheelock <eelirem@gmail.com>
2017-06-28 11:27:14 -0400

    water boiling temperature practice

--- src/App.js
+++ src/App.js
@@ -3,11 +3,32 @@ import './App.css';
 import $ from 'jquery';
 import FilterableProductTable from './FilterableProductTable';
 
+import Toggle from './Toggle';
+import MyForm from './MyForm';
+import Calculator from './Calculator';
+
 class App extends Component {
     constructor(props){
-        super(props)
+        super(props);
     }
 	render() {
+
+        // Water boiling app
+        return(
+            <Calculator />
+        )
+
+        // Practice Form
+        return(
+            <MyForm />
+        )
+
+        // Toggle practice
+        return(
+            <Toggle />
+        )
+
+        // Actual inventory app
 		return (
 			<div className="container App">
                 <div className="col-sm-4 col-sm-offset-4">
---
+++ src/Toggle.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Toggle extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			toggleOn: true
+		}
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick(){
+		this.setState({
+			toggleOn: !this.state.toggleOn
+		})
+	}
+
+	render(){
+		if(this.state.toggleOn){
+			var isToggleOn = "On"
+		}else{
+			var isToggleOn = "Off"
+		}
+		return(
+			<button onClick={this.handleClick}>
+				{isToggleOn}
+			</button>
+		)
+	}
+}
+
+export default Toggle;
